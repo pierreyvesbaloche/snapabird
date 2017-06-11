0ba2815725f2231bef9c3aad704b5e1a035b8d4b
Pierre-yves Baloche <funkypiwy@gmail.com>
2017-06-11 19:01:45 +0200

    Added proper shutdown method, based on subprocess call

--- snapabird/runner.py
+++ snapabird/runner.py
@@ -2,34 +2,67 @@
 # -*- coding: utf-8 -*-
 import gpiozero
 import logging
-import os
-
-PIN_STOP_BUTTON = 2
 
 
 class Runner(object):
+    """
+    Runner class for the SnapABird project.
+    """
+
+    # Command : Shutdown
+    CMD_SHUTDOWN = "/usr/bin/sudo /sbin/shutdown -h now"
+
+    # PIN : Stop Button
+    PIN_STOP_BUTTON = 2
+
+    # Setting up the log configuration
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(asctime)s %(threadName)s] %(message)s',
+                        datefmt='%H:%M:%S')
+
     def __init__(self, button):
         """
         Runner init method.
-        @param button: The button in charge of stopping the Runner.
-        @type button: int
+        @:param button: The button in charge of stopping the Runner.
+        @:type button: int
+        @:return None
         """
         self.button = button
         logging.debug("-- Constructor done with button "+str(self.button))
 
-    def run(self):
+    def shutdown(self, cmd):
+        """
+        Shutdown method to terminate completly the Pi's activity.
+        @:param cmd: The shutdown command to run.
+        @:type cmd: str
+        @:return The output of the command.
+        @:rtype str if defined, None otherwise.
+        """
+        if cmd:
+            logging.info("-- __shutdown() started")
+            import subprocess
+            process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+            logging.info("-- __shutdown() processing with %s".format(process))
+            output = process.communicate()[0]
+            logging.info("-- __shutdown() done with %s".format(output))
+            return output
+        else:
+            logging.warning("-- __shutdown() done with no command.")
+            return None
+
+    def process(self):
         """
         Main method of the runner.
-        @return:
+        @:return None
         """
-        logging.info("-- run() started")
+        logging.info("-- process() started")
         self.button.wait_for_press()
-        logging.info("-- run() done")
-        os.system("shutdown now -h")
+        logging.info("-- process() done")
+        self.shutdown(self.CMD_SHUTDOWN)
 
 if __name__ == '__main__':  # pragma: no cover
     """
     Main Loop.
     """
-    runner = Runner(gpiozero.Button(PIN_STOP_BUTTON))
-    runner.run()
+    runner = Runner(gpiozero.Button(Runner.PIN_STOP_BUTTON))
+    runner.process()
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -14,16 +14,37 @@ class RunnerTestCase(unittest.TestCase):
     """
 
     @mock.patch("gpiozero.Button")
-    @mock.patch("os.system")
-    def test_run(self, mock_system, mock_button):
+    @mock.patch("subprocess.Popen")
+    def test_run(self, mock_sub_process, mock_button):
         """
         Check the runner's main function.
-        :param mock_system: The fake system call.
+        :param mock_sub_process: The fake system call.
         :param mock_button: The button used by the runner.
         :return:
         """
         logging.debug("Mock is " + str(mock_button))
         test_runner = runner.Runner(mock_button)
-        test_runner.run()
+        test_runner.process()
         mock_button.wait_for_press.assert_called()
-        mock_system.assert_called()
+        mock_sub_process.assert_called()
+
+    def test_shutdown_command(self):
+        """
+        Check the runner's shutdown system command.
+        :return:
+        """
+        self.assertEqual("/usr/bin/sudo /sbin/shutdown -h now", runner.Runner.CMD_SHUTDOWN)
+
+    @mock.patch("gpiozero.Button")
+    @mock.patch("subprocess.Popen")
+    def test_shutdown_method_ko(self, mock_sub_process, mock_button):
+        """
+        Check the runner's shutdown system command.
+        :param mock_sub_process: The fake system call.
+        :param mock_button: The button used by the runner.
+        :return:
+        """
+        test_runner = runner.Runner(mock_button)
+        test_runner.shutdown(None)
+        mock_sub_process.assert_not_called()
+
